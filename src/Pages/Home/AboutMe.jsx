1bf77f82292f1a21674e025b110a4710b5aa4ce4
larryprochko <lawrencep123@hotmail.com>
2025-01-12 22:44:46 -0500

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,20 @@
+export default function AboutMe(){
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="AboutMe" />
+            </div>
+            <div className="hero--section--content--boxabout--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title"></p> 
+                    <h1 className="skills-section--heading">About Me 
+                    </h1>
+                    <p className="hero--section-description">I am Computer Science student with a passion for continuous learning and innovation. My interests span across cybersecurity, penetration testing, artificial intelligence, and networking, and I’m actively working on projects in these areas to expand my skills and knowledge.
+                    </p>
+                    <p className="hero--section-description">As a member of my school’s cybersecurity team, I thrive on solving complex problems and collaborating with others to tackle challenges in the ever-evolving world of technology. With a strong commitment to growth, I’m excited to connect with like-minded professionals and explore opportunities to make an impact in the tech industry.
+                    </p>
+                </div>
+            </div>
+    </section>
+    )
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+    return (
+      <section id="heroSection" className="hero--section">
+        <div className="hero--section--content--box">
+          <div className="hero--section--content">
+            <p className="section--title">Hey, I'm Lawrence</p>
+            <h1 className="hero--section--title">
+              <span className="hero--section-title--color"> Aspiring Pentester and Cloud Engineer | HTB Learner with a Focus on Network Security</span>{" "}
+              <br />
+              {/* Add send list here for titles  */}
+            </h1>
+            <p className="hero--section-description">
+              {/* Add intro here  */}
+              <br /> 
+            </p>
+          </div>
+          
+        </div>
+        <div className="hero--section--img">
+          <img src="./img/hero_img.png" alt="Hero Section" />
+        </div>
+      </section>
+    );
+  }
